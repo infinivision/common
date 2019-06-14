01254e8d0cc23a37285eef0f4caf1fed58e6eb83
root <nnsmgsone@gmail.com>
2019-06-14 15:50:16 +0800

    Add Big Endian interger encode and decode

--- miscellaneous/miscellaneous.go
+++ miscellaneous/miscellaneous.go
@@ -1,32 +1,12 @@
 package miscellaneous
 
 import (
+	"encoding/binary"
 	"errors"
 
 	"github.com/infinivision/common/slice"
 	"github.com/infinivision/common/typeclass"
 )
-
-type uint8ToByteFunc (func(uint8) []byte)
-type byteToUint8Func (func([]byte) (uint8, error))
-
-type uint16ToByteFunc (func(uint16) []byte)
-type byteToUint16Func (func([]byte) (uint16, error))
-
-type uint32ToByteFunc (func(uint32) []byte)
-type byteToUint32Func (func([]byte) (uint32, error))
-
-type uint64ToByteFunc (func(uint64) []byte)
-type byteToUint64Func (func([]byte) (uint64, error))
-
-var E8func uint8ToByteFunc
-var D8func byteToUint8Func
-var E16func uint16ToByteFunc
-var D16func byteToUint16Func
-var E32func uint32ToByteFunc
-var D32func byteToUint32Func
-var E64func uint64ToByteFunc
-var D64func byteToUint64Func
 
 func Max(x, y int) int {
 	switch {
@@ -46,182 +26,103 @@ func Min(x, y int) int {
 	}
 }
 
-func init() { // 初始化整型序列化函数
-	buf := uint32ToBytes(1)
-	E8func = uint8ToBytes
-	D8func = bytesToUint8
-	if buf[0] == 0 {
-		E16func = bigEndianUint16ToBytes
-		D16func = bigEndianBytesToUint16
-		E32func = bigEndianUint32ToBytes
-		D32func = bigEndianBytesToUint32
-		E64func = bigEndianUint64ToBytes
-		D64func = bigEndianBytesToUint64
-	} else {
-		E16func = uint16ToBytes
-		D16func = bytesToUint16
-		E32func = uint32ToBytes
-		D32func = bytesToUint32
-		E64func = uint64ToBytes
-		D64func = bytesToUint64
-	}
-}
-
-// big endian to little endian
-func bigEndianUint16ToBytes(a uint16) []byte {
-	buf := make([]byte, 2)
-	buf[1] = byte(a & 0xFF)
-	buf[0] = byte((a >> 8) & 0xFF)
-	return buf
-}
-
-func bigEndianBytesToUint16(a []byte) (uint16, error) {
-	if len(a) != 2 {
-		return 0, errors.New("bigEndianBytesToUint16: Illegal slice length")
-	}
-	b := uint16(0)
-	for i, v := range a {
-		b += uint16(v)
-		if i > 0 {
-			b <<= 8
-		}
-	}
-	return b, nil
-}
-
-// big endian to little endian
-func bigEndianUint32ToBytes(a uint32) []byte {
-	buf := make([]byte, 4)
-	buf[3] = byte(a & 0xFF)
-	buf[2] = byte((a >> 8) & 0xFF)
-	buf[1] = byte((a >> 16) & 0xFF)
-	buf[0] = byte((a >> 24) & 0xFF)
-	return buf
-}
-
-func bigEndianBytesToUint32(a []byte) (uint32, error) {
-	if len(a) != 4 {
-		return 0, errors.New("bigEndianBytesToUint32: Illegal slice length")
-	}
-	b := uint32(0)
-	for i, v := range a {
-		b += uint32(v)
-		if i > 0 {
-			b <<= 8
-		}
-	}
-	return b, nil
-}
-
-// big endian to little endian
-func bigEndianUint64ToBytes(a uint64) []byte {
-	buf := make([]byte, 8)
-	buf[7] = byte(a & 0xFF)
-	buf[6] = byte((a >> 8) & 0xFF)
-	buf[5] = byte((a >> 16) & 0xFF)
-	buf[4] = byte((a >> 24) & 0xFF)
-	buf[3] = byte((a >> 32) & 0xFF)
-	buf[2] = byte((a >> 40) & 0xFF)
-	buf[1] = byte((a >> 48) & 0xFF)
-	buf[0] = byte((a >> 56) & 0xFF)
-	return buf
-}
-
-func bigEndianBytesToUint64(a []byte) (uint64, error) {
-	if len(a) != 8 {
-		return 0, errors.New("bigEndianBytesToUint64: Illegal slice length")
-	}
-	b := uint64(0)
-	for i, v := range a {
-		b += uint64(v)
-		if i > 0 {
-			b <<= 8
-		}
-	}
-	return b, nil
-}
-
-func uint8ToBytes(a uint8) []byte {
-	buf := make([]byte, 1)
-	buf[0] = a & 0xFF
-	return buf
-}
-
-func bytesToUint8(a []byte) (uint8, error) {
-	if len(a) != 1 {
-		return 0, errors.New("BytesToUint8: Illegal slice length")
-	}
-	return uint8(a[0]), nil
-}
-
-func uint16ToBytes(a uint16) []byte {
-	buf := make([]byte, 2)
-	buf[0] = byte(a & 0xFF)
-	buf[1] = byte((a >> 8) & 0xFF)
-	return buf
-}
-
-func bytesToUint16(a []byte) (uint16, error) {
-	if len(a) != 2 {
-		return 0, errors.New("BytesToUint16: Illegal slice length")
-	}
-	b := uint16(0)
-	for i, v := range a {
-		b += uint16(v) << (8 * uint16(i))
-	}
-	return b, nil
-}
-
-func uint32ToBytes(a uint32) []byte {
-	buf := make([]byte, 4)
-	buf[0] = byte(a & 0xFF)
-	buf[1] = byte((a >> 8) & 0xFF)
-	buf[2] = byte((a >> 16) & 0xFF)
-	buf[3] = byte((a >> 24) & 0xFF)
-	return buf
-}
-
-func bytesToUint32(a []byte) (uint32, error) {
-	if len(a) != 4 {
-		return 0, errors.New("bytesToUint32: Illegal slice length")
-	}
-	b := uint32(0)
-	for i, v := range a {
-		b += uint32(v) << (8 * uint32(i))
-	}
-	return b, nil
-}
-
-func uint64ToBytes(a uint64) []byte {
-	buf := make([]byte, 8)
-	buf[0] = byte(a & 0xFF)
-	buf[1] = byte((a >> 8) & 0xFF)
-	buf[2] = byte((a >> 16) & 0xFF)
-	buf[3] = byte((a >> 24) & 0xFF)
-	buf[4] = byte((a >> 32) & 0xFF)
-	buf[5] = byte((a >> 40) & 0xFF)
-	buf[6] = byte((a >> 48) & 0xFF)
-	buf[7] = byte((a >> 56) & 0xFF)
-	return buf
-}
-
-func bytesToUint64(a []byte) (uint64, error) {
-	if len(a) != 8 {
-		return 0, errors.New("bytesToUint64: Illegal slice length")
-	}
-	b := uint64(0)
-	for i, v := range a {
-		b += uint64(v) << (8 * uint64(i))
-	}
-	return b, nil
-}
-
 func Dup(a []byte) []byte {
 	if a == nil {
 		return nil
 	}
 	b := []byte{}
 	return append(b, a...)
+}
+
+func E8func(a uint8) []byte {
+	buf := make([]byte, 1)
+	buf[0] = a & 0xFF
+	return buf
+}
+
+func D8func(a []byte) (uint8, error) {
+	if len(a) != 1 {
+		return 0, errors.New("D8func: Illegal slice length")
+	}
+	return uint8(a[0]), nil
+}
+
+func E16func(a uint16) []byte {
+	buf := make([]byte, 2)
+	binary.LittleEndian.PutUint16(buf, a)
+	return buf
+}
+
+func D16func(a []byte) (uint16, error) {
+	if len(a) != 2 {
+		return 0, errors.New("D16func: Illegal slice length")
+	}
+	return binary.LittleEndian.Uint16(a), nil
+}
+
+func E32func(a uint32) []byte {
+	buf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buf, a)
+	return buf
+}
+
+func D32func(a []byte) (uint32, error) {
+	if len(a) != 4 {
+		return 0, errors.New("D32func: Illegal slice length")
+	}
+	return binary.LittleEndian.Uint32(a), nil
+}
+
+func E64func(a uint64) []byte {
+	buf := make([]byte, 8)
+	binary.LittleEndian.PutUint64(buf, a)
+	return buf
+}
+
+func D64func(a []byte) (uint64, error) {
+	if len(a) != 8 {
+		return 0, errors.New("D64func: Illegal slice length")
+	}
+	return binary.LittleEndian.Uint64(a), nil
+}
+
+func EB16func(a uint16) []byte {
+	buf := make([]byte, 2)
+	binary.BigEndian.PutUint16(buf, a)
+	return buf
+}
+
+func DB16func(a []byte) (uint16, error) {
+	if len(a) != 2 {
+		return 0, errors.New("DB16func: Illegal slice length")
+	}
+	return binary.BigEndian.Uint16(a), nil
+}
+
+func EB32func(a uint32) []byte {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, a)
+	return buf
+}
+
+func DB32func(a []byte) (uint32, error) {
+	if len(a) != 4 {
+		return 0, errors.New("DB32func: Illegal slice length")
+	}
+	return binary.BigEndian.Uint32(a), nil
+}
+
+func EB64func(a uint64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, a)
+	return buf
+}
+
+func DB64func(a []byte) (uint64, error) {
+	if len(a) != 8 {
+		return 0, errors.New("DB64func: Illegal slice length")
+	}
+	return binary.BigEndian.Uint64(a), nil
 }
 
 // slice length || slice
